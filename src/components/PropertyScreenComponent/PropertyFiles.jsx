460eff32b8d7c384d15aba91eebf2a717da3f7c0
francis19-bit <124472074+francis19-bit@users.noreply.github.com>
2023-09-12 20:06:28 -0400

    PropertyTasks

--- src/components/PropertyScreenComponent/PropertyFiles.jsx
+++ src/components/PropertyScreenComponent/PropertyFiles.jsx
@@ -1,7 +1,59 @@
+import { Assignment, Download } from "@mui/icons-material";
+import { Card, IconButton } from "@mui/material";
 import React from "react";
 
 const PropertyFiles = () => {
-  return <div style={{ height: "57vh" }}>PropertyFiles</div>;
+  return (
+    <div style={{ height: "57vh" }}>
+      <Card
+        sx={{
+          width: "15vw",
+          height: "15vh",
+          borderRadius: "3%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            flex: ".4",
+
+            marginTop: "2vh",
+            display: "flex",
+          }}
+        >
+          <div
+            style={{
+              flex: ".3",
+
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <IconButton>
+              {" "}
+              <Assignment />{" "}
+            </IconButton>
+          </div>
+          <div style={{ flex: "0.7", fontSize: ".6em", paddingTop: "1vh" }}>
+            {" "}
+            <span style={{ fontWeight: "600" }}>
+              Identify proof docunents{" "}
+            </span>{" "}
+            <br /> 1.2mb
+          </div>
+        </div>
+        <div style={{ flex: ".4", display: "flex" }}>
+          <div style={{ flex: "0.3" }}> </div>
+          <div style={{ flex: "0.7" }}>
+            {" "}
+            <Download sx={{ width: 15 }} />{" "}
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default PropertyFiles;
--- src/components/PropertyScreenComponent/PropertyNotes.jsx
+++ src/components/PropertyScreenComponent/PropertyNotes.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "../../App.css";
 
 const PropertyNotes = () => {
-  return <div style={{ height: "57vh" }}>PropertyNotes</div>;
+  return (
+    <div style={{ height: "57vh" }}>
+      <div className="Propertynotes">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default PropertyNotes;
